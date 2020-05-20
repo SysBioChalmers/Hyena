655bf661a1b6f9dd662bfd18b5577b1144e12e3f
Christoph Boerlin <borlinc@chalmers.se>
2020-05-20 10:12:15 +0200

    Simplified data loading

--- Hyena_Create_IntervalData_Features.py
+++ Hyena_Create_IntervalData_Features.py
@@ -10,12 +10,13 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-def parseWigLike(selected_genes, selected_tf, path, date):
+def parseWigLike(selected_genes, selected_tf):
     #load data
-    data_Glu=[[x for x in line.rstrip('\n\r').split('\t')] for line in open(path + selected_tf+'_Glu_ol_combRep_geneAssigned_' + date + '.wigLike')]
+    path_to_ChIPexo = 'Data_ChIPexo/'
+    data_Glu=[[x for x in line.rstrip('\n\r').split('\t')] for line in open(path_to_ChIPexo + selected_tf+'_Glu.wigLike')]
     data_Glu = {x[0] + '_' + x[1] : float(x[2]) for x in data_Glu if x[0] in selected_genes}
     
-    data_Eth=[[x for x in line.rstrip('\n\r').split('\t')] for line in open(path + selected_tf+'_Eth_ol_combRep_geneAssigned_' + date + '.wigLike')]
+    data_Eth=[[x for x in line.rstrip('\n\r').split('\t')] for line in open(path_to_ChIPexo + selected_tf+'_Eth.wigLike')]
     data_Eth = {x[0] + '_' + x[1] : float(x[2]) for x in data_Eth if x[0] in selected_genes}
     
     combined_keys = list(set(list(data_Glu.keys()) + list(data_Eth.keys())))
@@ -51,12 +52,7 @@ def below_zero(data):
 
 print('Start loading and creating features - ' + datetime.now().strftime('%H:%M:%S'))
 
-main_chip_path = '../180126_ChIPexo_CENPK/Results_190402/'
-main_chip_date = '190314'
-selected_tfs = {x : [main_chip_path, main_chip_date] for x in ['Cat8','Cbf1','Ert1','Gcn4','Gcr1','Gcr2','Hap1','Ino2','Ino4','Leu3','Oaf1','Pip2','Rds2','Rgt1','Rtg1','Rtg3','Sip4','Stb5','Sut1','Tye7']}
-
-#add Cst6
-selected_tfs['Cst6'] = ['../191203_Cst6_ChIPexo/', '191203']
+selected_tfs = ['Cat8','Cbf1', 'Cst6', 'Ert1', 'Gcn4', 'Gcr1', 'Gcr2', 'Hap1', 'Ino2', 'Ino4', 'Leu3', 'Oaf1', 'Pip2', 'Rds2', 'Rgt1', 'Rtg1', 'Rtg3', 'Sip4', 'Stb5', 'Sut1', 'Tye7']
 
 #Choose a gene list
 selected_genes = [line.strip('\r\n') for line in open('Data/GenesWithTSSAnnotation_Yeast82.csv')]
@@ -75,11 +71,9 @@ exp_data = np.array(exp_data.Ratio)
 
 #load tf data
 tf_data = pd.DataFrame(columns = ['Gene', 'TF', 'Pos', 'Glu_Value', 'Eth_Value', 'Diff_Value'])
-for selected_tf, (path, date) in selected_tfs.items():
-        tf_data = tf_data.append(parseWigLike(selected_genes, selected_tf, path, date))
-print('Done loading and data - ' + datetime.now().strftime('%H:%M:%S'))
-
-
+for selected_tf in selected_tfs:
+        tf_data = tf_data.append(parseWigLike(selected_genes, selected_tf))
+print('Done loading data - ' + datetime.now().strftime('%H:%M:%S'))
 
 tf_data_intervals = pd.DataFrame()
 
@@ -102,7 +96,7 @@ tf_data_intervals.sort_values(by = ['Gene', 'TF', 'Interval'], inplace = True, i
 tf_data_intervals = tf_data_intervals.round(decimals = 3)
 
 #set multiindex
-tf_data_intervals.set_index(['Gene', 'TF', 'Interval'], inplace = True)
+#tf_data_intervals.set_index(['Gene', 'TF', 'Interval'], inplace = True)
 
 #melt it for reordering
 tf_data_intervals = pd.melt(tf_data_intervals, id_vars = ['Gene', 'TF', 'Interval'],
@@ -114,7 +108,7 @@ types = ['Glu_sum', 'Glu_az', 'Eth_sum', 'Eth_az', 'Diff_sum', 'Diff_az', 'Diff_
 tf_data_wide = pd.DataFrame(columns = ['Interval', 'TF', 'Type'] + selected_genes)
 tf_data_wide_list = []
 for i in range(-1000,500,50):
-    for tf in selected_tfs.keys():
+    for tf in selected_tfs:
         for type_name in types:
                 tf_data_wide_tmp = tf_data_intervals.loc[
                     (tf_data_intervals.TF == tf) & 
@@ -131,7 +125,7 @@ for i in range(-1000,500,50):
 tf_data_wide = tf_data_wide.append(tf_data_wide_list)
 tf_data_wide.fillna(0, inplace = True)
 tf_data_wide.reset_index(inplace = True)
-tf_data_wide.drop(column = 'Value', inplace = True)
+tf_data_wide.drop(columns = 'index', inplace = True)
 
 #add sum for all TFs
 tf_data_wide_alltfs = pd.DataFrame(columns = tf_data_wide.columns)
@@ -149,8 +143,9 @@ tf_data_wide = tf_data_wide.append(tf_data_wide_alltfs, ignore_index = True)
 
 #add sum for TF groups
 tf_data_wide_tfgroups = pd.DataFrame(columns = tf_data_wide.columns)
-groups = {'ZincCluster' : ['Cat8', 'Ert1', 'Hap1', 'Leu3', 'Sip4', 'Oaf1', 'Pip2', 'Rds2', 'Sut1', 'Rgt1', 'Stb5'],
-          'Zipper' : ['Cbf1', 'Ino2', 'Ino4', 'Tye7', 'Rtg1', 'Rtg3', 'Gcn4', 'Gcr1', 'Gcr2']}
+groups = {'ZincCluster' : ['Cat8', 'Ert1', 'Hap1', 'Leu3', 'Oaf1', 'Pip2', 'Rds2', 'Rgt1', 'Sip4', 'Sut1', 'Stb5'],
+          'Zipper' : ['Cbf1', 'Gcn4', 'Ino2', 'Ino4', 'Met4', 'Rtg1', 'Rtg3', 'Tye7']}
+
 for i in range(-1000,500,50):
     for group, group_tfs in groups.items():
         for type_name in types:
@@ -208,7 +203,7 @@ tf_data_proc = tf_data_proc.transpose()
 tf_data_proc['exp_data'] = exp_data
 tf_data_proc.to_csv('Data/CombinedTFdata_features.csv')
 
-
+print('Done creating features - ' + datetime.now().strftime('%H:%M:%S'))
     
     
     
